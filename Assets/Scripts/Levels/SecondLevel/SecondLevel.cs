7de0272d538fdcd6d2f17563f0f2bcb1689c9a45
Benjamin Rappo <benjamin.rappo69@gmail.com>
2023-12-27 19:15:52 +0100

    added seconelvel script

---
+++ Assets/Scripts/Levels/SecondLevel/SecondLevel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondLevel : Level
+{
+    protected override void SecondStart()
+    {
+
+    }
+
+    protected override void SecondUpdate()
+    {
+
+    }
+
+    protected override void WinAction()
+    {
+
+    }
+
+    protected override void LoseAction()
+    {
+
+    }
+
+    protected override void WakeUpAction()
+    {
+
+    }
+}
